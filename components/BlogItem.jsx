19618c1fb932708be5387141c758456fa48cea7b
UdariSathsarani1001 <chethanasathsarani1001@gmail.com>
2026-03-01 12:41:53 +0530

    feat: Add blog seeding script, `BlogItem` component, and initial blog image assets.

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const BlogItem = ({ title, description, image, category, id }) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-      <Link href={`/Blogs/${id}`}>
+      <Link href={`/blogs/${id}`}>
         <Image src={image} alt='' width={400} height={400} className='border-b border-black' />
       </Link>
       <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
@@ -15,7 +15,7 @@ const BlogItem = ({ title, description, image, category, id }) => {
         <p className='mb-3 text-sm tracking-tight text-gray-700'
           dangerouslySetInnerHTML={{ __html: description.slice(0, 120) }}
         ></p>
-        <Link href={`/Blogs/${id}`} className="inline-flex items-center py-2 font-semibold text-center">
+        <Link href={`/blogs/${id}`} className="inline-flex items-center py-2 font-semibold text-center">
           Read more <Image src={assets.arrow} className='ml-2' alt='' />
         </Link>
       </div>
